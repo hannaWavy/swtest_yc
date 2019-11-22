e8abff3c39cc342995ab309b1ddb1668794d192f
Wavyline <hannawavy@gmail.com>
2019-11-22 17:54:11 +0200

    new test

--- js/sw.js
+++ js/sw.js
@@ -1,16 +1,24 @@
-var deferredPrompt;
+// var deferredPrompt;
 
-console.log('serv working');
+// self.addEventListener('beforeinstallprompt', function(e) {
+//     deferredPrompt = e;
+//     alert('not installed');
+// });
 
-self.addEventListener('beforeinstallprompt', function(e) {
-    deferredPrompt = e;
-    alert('not installed');
-});
-
-self.addEventListener('appinstalled', function(e) { //if the app is on home screen
-  alert('yc installed');
-});
+// self.addEventListener('appinstalled', function(e) { //if the app is on home screen
+//   alert('yc installed');
+// });
 
 self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
