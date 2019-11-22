50b63d18d50d3e67b71c44413d03d0a093c5cc22
Wavyline <hannawavy@gmail.com>
2019-11-22 17:35:41 +0200

    new test

--- js/sw.js
+++ js/sw.js
@@ -1,5 +1,7 @@
 var deferredPrompt;
 
+console.log('serv working');
+
 self.addEventListener('beforeinstallprompt', function(e) {
     deferredPrompt = e;
     alert('not installed');
@@ -7,4 +9,8 @@ self.addEventListener('beforeinstallprompt', function(e) {
 
 self.addEventListener('appinstalled', function(e) { //if the app is on home screen
   alert('yc installed');
+});
+
+self.addEventListener('fetch', function(event) {
+    console.log(event.request.url);
 });
