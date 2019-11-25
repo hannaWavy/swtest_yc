0bf8cd8e64e3b087b4f1e1cc80b31e51602c3b66
Wavyline <hannawavy@gmail.com>
2019-11-25 18:07:52 +0200

    new way

--- js/sw.js
+++ js/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = 'my-site-cache-v1';
+var CACHE_NAME = 'yc-v1';
 var urlsToCache = [
   '/'
 ];
@@ -8,7 +8,6 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
-        console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
   );
