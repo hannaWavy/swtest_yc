15e29a3c05db0009db067653dd129e484a3a34f5
Wavyline <hannawavy@gmail.com>
2021-04-21 13:37:27 +0300

    remove old

--- sw.js
+++ sw.js
@@ -26,3 +26,48 @@ self.addEventListener('fetch', function(event) {
     )
   );
 });
+
+/*
+    var deferredPrompt = false;
+    var messageBlock = document.getElementById('messageBlock');
+    var btnAdd = document.getElementById('btnAdd');
+
+
+    if('serviceWorker' in navigator) {
+        navigator.serviceWorker
+            .register('./sw.js')
+            // .then(function(registration) {});
+            .then(function(registration) { console.log('ServiceWorker registration successful with scope: ', registration.scope); });
+    }
+
+    window.addEventListener('appinstalled', function() {
+        messageBlock.textContent = "install_chrome_pwa";
+    });
+
+    window.addEventListener('beforeinstallprompt', function(e) {
+        e.preventDefault();
+
+        deferredPrompt = e;
+        btnAdd.style.display = "block";
+
+        return false;
+    });
+
+    btnAdd.addEventListener('click', function(e) {
+        if (deferredPrompt) {
+            // hide our user interface that shows our A2HS button
+            btnAdd.style.display = 'none';
+            // Show the prompt
+            deferredPrompt.prompt();
+            console.log("add_chrome_banner");
+            // Wait for the user to respond to the prompt
+            deferredPrompt.userChoice.then((choiceResult) => {
+                if (choiceResult.outcome === 'accepted') {
+                    console.log("add_chrome_banner");   
+                } else {
+                    console.log("close_chrome_banner");
+                }
+                deferredPrompt = null;
+            });
+        }
+    }); */
