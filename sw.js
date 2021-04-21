dd27aa7e38f641f6b92efdf13d0297c02f5525dd
Wavyline <hannawavy@gmail.com>
2021-04-21 15:25:52 +0300

    new sw with cache

--- sw.js
+++ sw.js
@@ -1,33 +1,69 @@
-var CACHE_NAME = 'yc-cache-v1';
-// var urlsToCache = [
-//   '/swtest_yc/index.html'
-// ];
-
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        console.log(event);
-        console.log(event.request);
-        return fetch(event.request);
-      }
-    )
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/images/example.png'
+      ]);
+    })
   );
 });
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
+      return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
+        let responseClone = response.clone();
+        
+        caches.open('v1').then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      }).catch(function () {
+        return caches.match('/images/example.png');
+      });
+    }
+  }));
+});
+
+// var CACHE_NAME = 'yc-cache-v1';
+// // var urlsToCache = [
+// //   '/swtest_yc/index.html'
+// // ];
+
+// // self.addEventListener('install', function(event) {
+// //   // Perform install steps
+// //   event.waitUntil(
+// //     caches.open(CACHE_NAME)
+// //       .then(function(cache) {
+// //         return cache.addAll(urlsToCache);
+// //       })
+// //   );
+// // });
+
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         console.log(event);
+//         console.log(event.request);
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
 
 /*
     var deferredPrompt = false;
